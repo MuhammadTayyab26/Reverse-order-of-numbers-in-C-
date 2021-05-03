beb1587538645c770244af1e892224fb15100c36
MuhammadTayyab26 <bcsm-f16-526@superior.edu.pk>
2021-05-04 01:11:36 +0500

    Reverse order of numbers in C++

---
+++ reverse.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <conio.h> 
+#include <iostream>
+using namespace std;
+int main() 
+{ 
+int a,b,c,d,e;
+
+cout << "Enter first number: ";
+cin >> a;
+
+cout << "\nEnter second number: ";
+cin >> b;
+
+cout << "\nEnter third number: ";
+cin >> c;
+
+cout << "\nEnter fourth number ";
+cin >> d;
+
+cout << "\nEnter five number ";
+cin >> e;
+
+cout <<"\n\n";
+
+cout << "Actual Number: " << a << b << c << d << e;
+cout << "\nReverse Order: " << e << d << c << b << a;
+
+getch();
+}
